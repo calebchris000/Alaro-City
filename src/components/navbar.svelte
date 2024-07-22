b2ad5d91d31bc4291b800b2d3a1aeec863b0ffb9
Caleb Nwaizu <calebchris000@gmail.com>
2024-07-22 12:22:24 +0100

    Setup navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import logo from "../assets/logo.png";
+</script>
+
+<nav
+    class="bg-gray-300 flex items-center h-20 px-4 gap-10 xl:px-32 xl:h-16 justify-between"
+>
+    <div class="w-28"><img class="w-full" src={logo} alt="" /></div>
+    <div>
+        <a class="font-medium" href="#contact">CONTACT US</a>
+    </div>
+    <div class="hidden xl:block"></div>
+</nav>
---
+++ src/pages/landing.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Navbar from "../components/navbar.svelte";
+</script>
+
+<main class="main">
+    <Navbar />
+</main>
